2f0a8328f674f2b18656ed436da1c95315765a41
Bharath746 <bharathjoshigolle@outlook.com>
2024-09-21 15:47:09 +0530

    App.componrent.html space

---
+++ src/app/departments/bariatric-surgery/bariatric-surgery.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bariatric-surgery',
+  templateUrl: './bariatric-surgery.component.html',
+  styleUrls: ['./bariatric-surgery.component.css'],
+})
+// export class Employee {
+//   empid: number;
+//   empname: string;
+//   salary: number;
+//   constructor(id: number, name: string, salary: number) {
+//     this.empid = id;
+//     this.empname = name;
+//     this.salary = salary;
+//   }
+// }
+export class BariatricSurgeryComponent implements OnInit {
+  title = 'ngforwithobjectarray';
+ 
+  ngOnInit(): void {}
+}
